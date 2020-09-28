d786e9f1df972f91d2bcada54e96338172ff2176
Eric <ericmiranda7@gmail.com>
2020-09-28 12:50:07 +0530

    add search by location

--- criminals/views.py
+++ criminals/views.py
@@ -5,25 +5,28 @@ from .forms import AdvancedSearch
 from django.views import View
 from django.views.generic import DetailView, ListView
 
+
 class CriminalsView(ListView):
     template_name = 'criminals/criminals.html'
     form_class = AdvancedSearch
     model = VerifiedCriminal
     paginate_by = 6
     context_object_name = 'criminals'
-    
 
     def get_queryset(self):
         name = self.kwargs.get('name', None)
         form = self.form_class(self.request.GET)
         if form.is_valid():
-            return self.model.objects.filter(name__contains=form.cleaned_data['name'])
+            return self.model.objects.filter(name__contains=form.cleaned_data['name']) \
+                .filter(state__name__contains=form.cleaned_data['state']) \
+                .filter(city__name__contains=form.cleaned_data['city'])
         return self.model.objects.all()
 
     def get_context_data(self, **kwargs):
         context = super(CriminalsView, self).get_context_data(**kwargs)
         context['advancedSearch'] = AdvancedSearch()
         return context
+
 
 class CriminalDetailView(DetailView):
     model = VerifiedCriminal
