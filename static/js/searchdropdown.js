c6027a25835952fc5e4dcb8eafdf9630cfefefcb
Eric <ericmiranda7@gmail.com>
2020-10-07 16:25:41 +0530

    add dropdown js

---
+++ static/js/searchdropdown.js
@@ -0,0 +1,15 @@
+// DROPDOWN JS
+stateSearch = document.querySelector('.state');
+stateSearch.addEventListener('focusout', removeCities)
+
+function removeCities() {
+    citySearch = document.querySelector('.city');
+    for (let i = 0; i < citySearch.options.length; i++) {
+        if (citySearch.options[i].value == stateSearch.value) {
+            citySearch.options[i].disabled = false;
+        } else {
+            citySearch.options[i].disabled = true;
+        }
+    }
+    console.log(citySearch)
+}
